522b1198e90f9da3b3575256c18154fa191cec0a
Ansuma Boro <161861184+ansumaboro@users.noreply.github.com>
2025-11-25 20:54:48 +0530

    Update cake.js

--- js/cake.js
+++ js/cake.js
@@ -134,6 +134,7 @@ Array.from(people).forEach(person =>{
             for(i=0; i<7; i++){
                 if(e.target.src.includes(persons[i])){
                     eatPerson.src = "images/friends/feed/"+persons[i]+".png"
+                    console.log(eatPerson.src);
                     feedCount[persons[i]]++;
                     animateEatPersonIn()
                 }
